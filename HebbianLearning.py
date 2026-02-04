f8577a98e37472ffce0035269d19bbb6bca68eca
Varad Mhetar <mhetarvarad@gmail.com>
2026-02-04 21:10:36 +0530

    Updated HebbianLearning.py

---
+++ HebbianLearning.py
@@ -0,0 +1,10 @@
+"""
+Docstring for HebbianLearning
+The Hebbian Learning module provides functions for implementing Hebbian Learning Algorithms.
+Hebbian learning is used in Neural Networks to provide a local learning rule inspired by the biological process of synaptic plasticity.
+"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
